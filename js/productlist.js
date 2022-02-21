f0925f003bda1aed6f3edcae3902eb39df597a53
MD <mdoremy@gmail.com>
2022-02-20 21:05:56 -0500

    Worked on productlist in HTML and javascript

---
+++ js/productlist.js
@@ -0,0 +1,21 @@
+/*var button1 = document.getElementById('button1');
+button1.addEventListener('click', function (event) {
+    console.log ('JavaScript Click Event', event);
+    alert('You Clicked Button 1');
+});*/
+(function () {
+    var database = [
+        {_id:'', name:'', genre:''},
+        {_id:'', name:'', genre:''},
+        {_id:'', name:'', genre:''},
+        {_id:'', name:'', genre:''},
+        {_id:'', name:'', genre:''},
+        {_id:'', name:'', genre:''},
+        {_id:'', name:'', genre:''},
+        {_id:'', name:'', genre:''},
+        {_id:'', name:'', genre:''},
+        {_id:'', name:'', genre:''},
+    ];
+
+    function renderList (results)
+})
